92a8a5eafa982cc02224141bd8db544c0bad2af2
HuyAtreides <phangiahuy2001@gmail.com>
2022-05-03 09:36:20 +0700

    Add docker file for database

---
+++ database/restore-database.sql
@@ -0,0 +1,9 @@
+﻿RESTORE DATABASE Book_Store 
+FROM DISK = "/usr/hamazon/database/Book_Store.bak"
+WITH FILE = 1,
+MOVE N'Book_Store' TO N'/var/opt/mssql/data/Book_Store.mdf',  
+MOVE N'Book_Store_Log' TO N'/var/opt/mssql/data/Book_Store_Log.ldf',
+NOUNLOAD,  REPLACE,  STATS = 1 
+
+
+
