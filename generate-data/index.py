6155f9904e281b77d54141ec127f9c9e1722f30d
HuyPhan <71184843+HuyAtreides@users.noreply.github.com>
2023-05-16 14:20:31 +0700

    Update index.py

--- generate-data/index.py
+++ generate-data/index.py
@@ -3,30 +3,30 @@ import json
 import re
 
 genres = [
-    # "history",
-    # "horror",
-    # "travel",
-    # "art",
-    # "business",
-    # "mystery",
-    # "fantasy",
-    # "thriller",
-    # "comics",
-    # "science-fiction",
-    # "non-fiction",
-    # "science",
-    # "biography",
+    "history",
+    "horror",
+    "travel",
+    "art",
+    "business",
+    "mystery",
+    "fantasy",
+    "thriller",
+    "comics",
+    "science-fiction",
+    "non-fiction",
+     "science",
+     "biography",
     "manga"
 ]
 
 book_list = []
 
 with open("books.json", "r+") as outfile:
-    # for genre in genres:
-    #     url = "https://www.goodreads.com/shelf/show/" + genre
-    #     books = get_book_list_page(url, genre)
-    #     book_list.extend(books)
+    for genre in genres:
+         url = "https://www.goodreads.com/shelf/show/" + genre
+         books = get_book_list_page(url, genre)
+         book_list.extend(books)
     file_data = json.load(outfile)
     print(len(file_data))
-    # file_data.extend(book_list)
-    # json.dump(file_data, outfile)
+     file_data.extend(book_list)
+     json.dump(file_data, outfile)
